52619110e31487a516b7f74405f446d353faccd1
illidan <smile@illidan.org>
2018-02-05 14:42:14 +0800

    修改客户端代码。实现Linux跨平台。

--- TCPClient1.0/Client.cpp
+++ TCPClient1.0/Client.cpp
@@ -1,9 +1,23 @@
-#define WIN32_LEAN_AND_MEAN
-
-#include <stdio.h>
+ï»¿#include <iostream>
 #include <thread>
+
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <WinSock2.h>
+#pragma comment(lib, "ws2_32.lib")
+#else
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+#define SOCKET int
+#define INVALID_SOCKET  (SOCKET)(~0)
+#define SOCKET_ERROR    (-1)
+#endif // _WIN32
+
+
 
 enum MGS_TYPE
 {
@@ -77,25 +91,23 @@ struct MsgNewUser : public MsgHeader
 
 int HandleMsg(SOCKET client)
 {
-	//½ÓÊÕ¿Í»§¶ËÇëÇó
+	//Cache Buffer
 	char buffer[1024] = {};
 	int size = recv(client, buffer, sizeof(MsgHeader), 0);
 	MsgHeader* request = (MsgHeader*)buffer;
 	if (SOCKET_ERROR == size)
 	{
-		printf("Error:½ÓÊÕ·þÎñÆ÷¶ËÇëÇó!\n"); //µãXÇ¿ÐÐ¹Ø±Õ¡£
+		printf("Error:Client off!\n");
 		return -1;
 	}
 	else if (size == 0)
 	{
-		printf("OK:·þÎñÆ÷¶Ë¹Ø±ÕÁ¬½Ó!\n");	 //ÓÃqÃüÁî¹Ø±Õ¡£
+		printf("OK:Client quit!\n");
 		return -2;
 	}
 
-	//ÏÔÊ¾¿Í»§¶ËÇëÇó
-	printf("--½ÓÊÕ·þÎñÆ÷¶ËÇëÇó£ºType:%d - Length:%d\n", request->msgType, request->msgLength);
+	printf("--Recieve Msg Type:%d - Length:%d\n", request->msgType, request->msgLength);
 
-	//´¦Àí¿Í»§¶ËÇëÇó
 	switch (request->msgType)
 	{
 	case MSG_LOGIN_RES:
@@ -103,7 +115,7 @@ int HandleMsg(SOCKET client)
 		recv(client, buffer + sizeof(MsgHeader), request->msgLength - sizeof(MsgHeader), 0);
 		MsgLoginRes* login = (MsgLoginRes*)buffer;
 
-		printf("--µÇÈëÏûÏ¢½á¹û£ºRet:%d\n", login->res);
+		printf("--Login Ret:%d\n", login->res);
 	}
 	break;
 	case MSG_LOGOUT_RES:
@@ -111,7 +123,7 @@ int HandleMsg(SOCKET client)
 		recv(client, buffer + sizeof(MsgHeader), request->msgLength - sizeof(MsgHeader), 0);
 		MsgLogoutRes* logout = (MsgLogoutRes*)buffer;
 
-		printf("--µÇ³öÏûÏ¢½á¹û£ºRet:%d\n", logout->res);
+		printf("--Logout Ret:%d\n", logout->res);
 	}
 	break;
 	case MSG_NEWUSER:
@@ -119,7 +131,7 @@ int HandleMsg(SOCKET client)
 		recv(client, buffer + sizeof(MsgHeader), request->msgLength - sizeof(MsgHeader), 0);
 		MsgNewUser* newUser = (MsgNewUser*)buffer;
 
-		printf("--ÐÂÓÃ»§<Socket=%d>¼ÓÈë·þÎñÆ÷!\n", newUser->user);
+		printf("--New User<Socket=%d>Join Server!\n", newUser->user);
 	}
 	break;
 	default:
@@ -148,22 +160,17 @@ void CmdThread(SOCKET socket)
 		}
 		else if (0 == strcmp(buffer, "Logout"))
 		{
+			gRun = false;
 			MsgLogout logout = {};
 			memcpy(logout.name, "illidan", sizeof("illidan"));
 			send(socket, (char*)&logout, sizeof(MsgLogout), 0);
-		}
-		else if (0 == strcmp(buffer, "Exit"))
-		{
-			gRun = false;
-			printf("OK:ÍË³ö!\n");
-			break;
 		}
 	}
 }
 
 int main()
 {
-	//´ò¿ªÍøÂç
+#ifdef _WIN32
 	WORD wsaVersion = MAKEWORD(2, 2);
 	WSADATA wsaData = {};
 	int iErrorNo = WSAStartup(wsaVersion, &wsaData);
@@ -171,31 +178,34 @@ int main()
 	{
 		printf("Error:WSA start up failed!\n");
 	}
+#endif // _WIN32
 
-	//´´½¨Socket
 	SOCKET _socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (SOCKET_ERROR == _socket)
 	{
-		printf("Error:´´½¨Socket!\n");
+		printf("Error:Can't Create Server!\n");
 	}
 	else
 	{
-		printf("OK:´´½¨Socket!\n");
+		printf("OK:Create Server!\n");
 	}
 
-	//Á¬½Ó·þÎñÆ÷
 	sockaddr_in sinServer = {};
 	sinServer.sin_family = AF_INET;
-	sinServer.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+#ifdef _WIN32
+	sinServer.sin_addr.S_un.S_addr = inet_addr("192.168.0.90");
+#else
+	sinServer.sin_addr.s_addr = inet_addr("192.168.0.90");
+#endif // _WIN32
 	sinServer.sin_port = htons(9090);
 	int sinLen = sizeof(sockaddr_in);
 	if (SOCKET_ERROR == connect(_socket, (sockaddr*)&sinServer, sinLen))
 	{
-		printf("Error:Á¬½Ó·þÎñÆ÷!\n");
+		printf("Error:Can't Connect Server!\n");
 	}
 	else
 	{
-		printf("OK:Á¬½Ó·þÎñÆ÷!\n");
+		printf("OK:Connect Server!\n");
 	}
 
 	std::thread t1(CmdThread, _socket);
@@ -212,7 +222,7 @@ int main()
 		int ret = select(0, &fdRead, NULL, NULL, &tv);
 		if (SOCKET_ERROR == ret)
 		{
-			printf("Error:Select´íÎó!\n");
+			printf("Error:Select Error!\n");
 			break;
 		}
 
@@ -223,30 +233,37 @@ int main()
 			int ret = HandleMsg(_socket);
 			if (ret < 0)
 			{
-				//·þÎñÆ÷´íÎóÍË³öÑ­»·
 				break;
 			}
 		}
 	}
 
-	//¹Ø±ÕÁ¬½Ó
+
+#ifdef _WIN32
 	if (SOCKET_ERROR == closesocket(_socket))
 	{
-		printf("Error:¹Ø±ÕÁ¬½Ó!\n");
+		printf("Error:!\n");
 	}
 	else
 	{
-		printf("OK:¹Ø±ÕÁ¬½Ó!\n");
+		printf("OK:!\n"); 
 	}
 
-	//¹Ø±ÕÍøÂç
 	int iError = WSACleanup();
 	if (SOCKET_ERROR == iError)
 	{
 		printf("Error:WSA clean up failed!\n");
 	}
+#else
+	if (SOCKET_ERROR == close(_socket))
+	{
+		printf("Error:!\n");
+	}
+	else
+	{
+		printf("OK:!\n");
+	}
+#endif // _WIN32
 
-	//ÍË³ö
-	getchar();
 	return 0;
 }
