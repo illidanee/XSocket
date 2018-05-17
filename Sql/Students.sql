4e85e68f0fbb71d5fa78a0baf32116fa167bafc6
illidan <smile@illidan.org>
2018-05-17 17:58:56 +0800

    添加文件

--- Sql/Students.sql
+++ Sql/Students.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2018-05-09 10:04:17
+Date: 2018-05-17 17:15:58
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,7 +20,7 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `Students`;
 CREATE TABLE `Students` (
-  `Device` varchar(64) DEFAULT '',
+  `Device` varchar(64) DEFAULT '0',
   `School` varchar(64) NOT NULL DEFAULT '',
   `Major` varchar(64) DEFAULT '',
   `StudentID` varchar(64) NOT NULL DEFAULT '',
@@ -31,13 +31,39 @@ CREATE TABLE `Students` (
   `DeviceName` varchar(64) DEFAULT '',
   `DeviceType` varchar(64) DEFAULT '',
   `MsgID` bigint(20) DEFAULT NULL,
-  UNIQUE KEY `SearchByStudentInfo` (`School`,`StudentID`) USING BTREE,
-  UNIQUE KEY `SearchByAccountInfo` (`UserName`,`Password`) USING BTREE
+  UNIQUE KEY `SearchByStudentInfo` (`School`,`StudentID`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of Students
 -- ----------------------------
-INSERT INTO `Students` VALUES ('dafe763dc7bc7121b3f83399aff2fe67acb568ed', 'aoyi', '11', '001', '11', '123', '110', '110', 'DESKTOP-N8F2QEI', 'Desktop', null);
-INSERT INTO `Students` VALUES ('dafe763dc7bc7121b3f83399aff2fe67acb568ed', 'aoyi', 'sd', '002', 'ds', '123456789101', '120', '111', 'DESKTOP-N8F2QEI', 'Desktop', null);
-INSERT INTO `Students` VALUES (null, 'aoyi', null, '003', null, null, null, null, null, null, null);
+INSERT INTO `Students` VALUES ('dafe763dc7bc7121b3f83399aff2fe67acb568ed', 'aoyi', '11', '000', '11', '1352254685', '110', '130', 'DESKTOP-N8F2QEI', 'Desktop', '36');
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '001', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '002', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '003', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '004', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '005', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '006', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '007', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '008', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '009', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '010', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '011', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '012', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '013', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '014', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '015', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '016', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '017', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '018', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '019', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '020', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '021', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '022', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '023', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '024', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '025', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '026', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '027', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '028', null, '', '', '', '', '', null);
+INSERT INTO `Students` VALUES ('0', 'aoyi', '', '029', null, '', '', '', '', '', null);
