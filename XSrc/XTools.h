ff4b8a05fff242991d6a037d21e053a138add6d4
illidan <smile@illidan.org>
2018-10-30 11:44:35 +0800

    New：添加编码转换工具。

---
+++ XSrc/XTools.h
@@ -0,0 +1,14 @@
+﻿#ifndef __XCONFIG_H__
+#define __XCONFIG_H__
+
+#include <string>
+
+class XTools
+{
+public:
+    static std::string GBKToUTF8(const std::string& strGBK);
+
+    static std::string UTF8ToGBK(const std::string& strUTF8);
+};
+
+#endif // ifndef
